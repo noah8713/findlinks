7311619a428a706bf5a13a1730f7f22d1bf41459
aginwala <aginwala@ebaysf.com>
2018-12-28 14:08:36 -0800

    Checking custom svc for kube

--- findlinks.go
+++ findlinks.go
@@ -4,55 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"golang.org/x/net/html"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:9099", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("q")
-	fmt.Fprintf(w, "Page = %q\n", url)
-	if len(url) == 0 {
+	svc_port := r.URL.Query().Get("svc_port")
+	svc_ip := r.URL.Query().Get("svc_ip")
+	fmt.Sprintf("svc url is http://%s:%s", svc_ip, svc_port)
+	if len(svc_ip) == 0 {
 		return
 	}
-	page, err := parse("https://" + url)
-	if err != nil {
-		fmt.Printf("Error getting page %s %s\n", url, err)
-		return
-	}
-	links := pageLinks(nil, page)
-	for _, link := range links {
-		fmt.Fprintf(w, "Link = %q\n", link)
-	}
-}
+	_, err := http.Get(fmt.Sprintf("http://%s:%s", svc_ip, svc_port))
 
-func parse(url string) (*html.Node, error) {
-	fmt.Println(url)
-	r, err := http.Get(url)
 	if err != nil {
-		return nil, fmt.Errorf("Cannot get page")
+		fmt.Fprint(w, "unReady")
+	}else {
+		fmt.Fprint(w, "Ready")
 	}
-	b, err := html.Parse(r.Body)
-	if err != nil {
-		return nil, fmt.Errorf("Cannot parse page")
-	}
-	return b, err
-}
 
-func pageLinks(links []string, n *html.Node) []string {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		for _, a := range n.Attr {
-			if a.Key == "href" {
-				links = append(links, a.Val)
-			}
-		}
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = pageLinks(links, c)
-	}
-	return links
 }
